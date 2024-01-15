6baea8d90d96fe72d6c92c8823ef43bc974a58d6
MeizCrazyLeader <110610631+MeizCrazyLeader@users.noreply.github.com>
2024-01-15 19:35:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os, time, math, shutil, pyromod.listen, asyncio, random, shlex
+import os, time, math, shutil, pyromod.listen, asyncio, random, shlex, re, requests
 from urllib.parse import unquote
 from urllib.error import HTTPError
 from pyrogram import Client, filters
@@ -202,199 +202,36 @@ async def start(bot, update):
 async def help(bot, update):
     await update.reply_text(f"How to use BulkLoader?!\n\n2 Methods:\n- send command /link and then send urls, separated by new line.\n- send txt file (links), separated by new line.", True, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
 
+def extract_links(input_url, output_file, excluded_extensions):
+    response = requests.get(input_url)
+    text = response.text
 
-@xbot.on_message(filters.command('link') & OWNER_FILTER & filters.private)
-async def linkloader(bot, update):
-    xlink = await update.chat.ask('Send your links, separated each link by new line', filters=filters.text, timeout=300)
-    if BUTTONS == True:
-        return await xlink.reply('Uploading methods.', True, reply_markup=InlineKeyboardMarkup(CB_BUTTONS))
-    elif BUTTONS == False:
-        pass
-    dirs = f'downloads/{update.from_user.id}'
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    output_filename = str(update.from_user.id)
-    filename = f'{dirs}/{output_filename}.zip'
-    pablo = await update.reply_text('Downloading...')
-    urlx = xlink.text.split('\n')
-    rm, total, up = len(urlx), len(urlx), 0
-    await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-    for url in urlx:
-        await download_file(url, dirs)
-        up+=1
-        rm-=1
-        try:
-            await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-        except BadRequest:
-            pass
-    await pablo.edit_text('Uploading...')
-    if AS_ZIP == True:
-        shutil.make_archive(output_filename, 'zip', dirs)
-        start_time = time.time()
-        await update.reply_document(
-            filename,
-            progress=progress_for_pyrogram,
-            progress_args=(
-                'Uploading...',
-                pablo,
-                start_time
-            )
-        )
-        await pablo.delete()
-        os.remove(filename)
-        shutil.rmtree(dirs)
-    elif AS_ZIP == False:
-        dldirs = [i async for i in absolute_paths(dirs)]
-        rm, total, up = len(dldirs), len(dldirs), 0
-        await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-        for files in dldirs:
-            await send_media(files, pablo)
-            up+=1
-            rm-=1
-            try:
-                await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-            except BadRequest:
-                pass
-            time.sleep(1)
-        await pablo.delete()
-        shutil.rmtree(dirs)
+    links = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+    
+    filtered_links = [link for link in links if not any(extension in link for extension in excluded_extensions)]
+    
+    with open(output_file, 'w') as file:
+        file.write('\n'.join(filtered_links))
 
+    print(f"✅ Links Extracted")
 
-@xbot.on_message(filters.document & OWNER_FILTER & filters.private)
-async def loader(bot, update):
-    if BUTTONS == True:
-        return await update.reply('You wanna upload files as?', True, reply_markup=InlineKeyboardMarkup(CB_BUTTONS))
-    elif BUTTONS == False:
-        pass
-    dirs = f'downloads/{update.from_user.id}'
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    if not update.document.file_name.endswith('.txt'):
-        return
-    output_filename = update.document.file_name[:-4]
-    filename = f'{dirs}/{output_filename}.zip'
-    pablo = await update.reply_text('Downloading...')
-    fl = await update.download()
-    with open(fl) as f:
-        urls = f.read()
-        urlx = urls.split('\n')
-        rm, total, up = len(urlx), len(urlx), 0
-        await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-        for url in urlx:
-            await download_file(url, dirs)
-            up+=1
-            rm-=1
-            try:
-                await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-            except BadRequest:
-                pass
-    await pablo.edit_text('Uploading...')
-    os.remove(fl)
-    if AS_ZIP == True:
-        shutil.make_archive(output_filename, 'zip', dirs)
-        start_time = time.time()
-        await update.reply_document(
-            filename,
-            progress=progress_for_pyrogram,
-            progress_args=(
-                'Uploading...',
-                pablo,
-                start_time
-            )
-        )
-        await pablo.delete()
-        os.remove(filename)
-        shutil.rmtree(dirs)
-    elif AS_ZIP == False:
-        dldirs = [i async for i in absolute_paths(dirs)]
-        rm, total, up = len(dldirs), len(dldirs), 0
-        await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-        for files in dldirs:
-            await send_media(files, pablo)
-            up+=1
-            rm-=1
-            try:
-                await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-            except BadRequest:
-                pass
-            time.sleep(1)
-        await pablo.delete()
-        shutil.rmtree(dirs)
+# Example usage
+input_url = input("📤 Enter Direct Link of TXT File: ")
+output_file = 'ace.txt'
+excluded_extensions = ['.html', '.srt', '.txt', '.png', '.jpg', '.jpeg', '.url', '.nfo', '.webp']  # Add the extensions you want to exclude in this list
 
+extract_links(input_url, output_file, excluded_extensions)
 
-@xbot.on_callback_query()
-async def callbacks(bot: Client, updatex: CallbackQuery):
-    cb_data = updatex.data
-    update = updatex.message.reply_to_message
-    await updatex.message.delete()
-    dirs = f'downloads/{update.from_user.id}'
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    if update.document:
-        output_filename = update.document.file_name[:-4]
-        filename = f'{dirs}/{output_filename}.zip'
-        pablo = await update.reply_text('Downloading...')
-        fl = await update.download()
-        with open(fl) as f:
-            urls = f.read()
-            urlx = urls.split('\n')
-            rm, total, up = len(urlx), len(urlx), 0
-            await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-            for url in urlx:
-                await download_file(url, dirs)
-                up+=1
-                rm-=1
-                try:
-                    await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-                except BadRequest:
-                    pass
-        os.remove(fl)
-    elif update.text:
-        output_filename = str(update.from_user.id)
-        filename = f'{dirs}/{output_filename}.zip'
-        pablo = await update.reply_text('Downloading...')
-        urlx = update.text.split('\n')
-        rm, total, up = len(urlx), len(urlx), 0
-        await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-        for url in urlx:
-            await download_file(url, dirs)
-            up+=1
-            rm-=1
-            try:
-                await pablo.edit_text(f"Total: {total}\nDownloaded: {up}\nDownloading: {rm}")
-            except BadRequest:
-                pass
-    await pablo.edit_text('Uploading...')
-    if cb_data == 'zip':
-        shutil.make_archive(output_filename, 'zip', dirs)
-        start_time = time.time()
-        await update.reply_document(
-            filename,
-            progress=progress_for_pyrogram,
-            progress_args=(
-                'Uploading...',
-                pablo,
-                start_time
-            )
-        )
-        await pablo.delete()
-        os.remove(filename)
-        shutil.rmtree(dirs)
-    elif cb_data == '1by1':
-        dldirs = [i async for i in absolute_paths(dirs)]
-        rm, total, up = len(dldirs), len(dldirs), 0
-        await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-        for files in dldirs:
-            await send_media(files, pablo)
-            up+=1
-            rm-=1
-            try:
-                await pablo.edit_text(f"Total: {total}\nUploaded: {up}\nUploading: {rm}")
-            except BadRequest:
-                pass
-            time.sleep(1)
-        await pablo.delete()
-        shutil.rmtree(dirs)
+# Upload the file to file.io
+file_io_url = 'https://file.io'
+files = {'file': open(output_file, 'rb')}
+response = requests.post(file_io_url, files=files)
+
+# Get the URL for accessing the file
+download_url = response.json()['link']
+
+print("📝 Conversion Completed 🎉")
+print(f"📩 Download: {download_url}")
 
 
 
